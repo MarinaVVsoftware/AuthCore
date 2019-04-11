88c39ba4c2ce2e323df967d82020cf5a5be17295
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-04-11 11:13:25 -0500

    #1 Fix de nombre de la ruta Auth

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,12 +2,12 @@
 // de routes, aquí se carga el arbol superior de las rutas y facilita
 // entender el ruteo de la API. También ayuda a el paso de parámetros de instancias
 // necesarias.
-const Example = require("../routes/Auth");
+const Auth = require("../routes/Auth");
 const Log = require("../helpers/Logs");
 
 module.exports = (app, router, firebaseAdmin) => {
   /* Rutas de login */
-  Example(app, router, firebaseAdmin);
+  Auth(app, router, firebaseAdmin);
 
   Log.Success("Rutas de la API cargadas.");
 };
