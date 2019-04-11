700e2e9131ed54e8278abffa4066472d4dc15af3
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-04-11 11:12:25 -0500

    #1 Endpoint POST: ValidateUser
    Este endpoint valida si el usuario existe o no en la db. Sirve como validación para la creación de usuarios.

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -28,4 +28,47 @@ Auth.CreateUser = function() {
   };
 };
 
+// POST: entrega información sobre si existe o no un usuario registrado.
+Auth.ValidateUser = function(firebaseAdmin) {
+  return function(req, res) {
+    // valida si se está enviando en el body el mail
+    if (!req.body.email)
+      return res
+        .status(400)
+        .send("No se ha especificado un email para validar.");
+
+    try {
+      firebaseAdmin
+        .auth()
+        .getUserByEmail(req.body.email)
+        .then(user => {
+          //si el usuario existe, arroja code:200 y un boolean true
+          res.status(200);
+          res.send(JSON.stringify({ user: true }));
+        })
+        .catch(error => {
+          if (error.code === "auth/user-not-found") {
+            //si el usuario no existe, arroja code:200 y un boolean false
+            res.status(200);
+            res.send(JSON.stringify({ user: false }));
+          } else {
+            Log.ErrorLog("Algo ha fallado en el servidor! Error: " + error);
+            res.status(500).send(
+              JSON.stringify({
+                error: "Algo ha fallado en el servidor! Error: " + error
+              })
+            );
+          }
+        });
+    } catch (error) {
+      Log.ErrorLog("Algo ha fallado en el servidor! Error: " + error);
+      res.status(500).send(
+        JSON.stringify({
+          error: "Algo ha fallado en el servidor! Error: " + error
+        })
+      );
+    }
+  };
+};
+
 module.exports = Auth;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,9 +4,10 @@
 // ejemplo de instancia de controlador
 const Auth = require("../controllers/Auth");
 
-module.exports = (app, router) => {
+module.exports = (app, router, firebaseAdmin) => {
   // ejemplo de ruta
   router.post("/api/auth/user", Auth.CreateUser());
+  router.post("/api/auth/validateUser", Auth.ValidateUser(firebaseAdmin));
 
   app.use(router);
 };
